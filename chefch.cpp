5fe6d67d5136152a11b773d2f442527e929132f7
rishirajsurti <rishirajsurti.iitm@gmail.com>
2015-02-12 21:11:48 +0530

    feb challenge

--- chefch.cpp
+++ chefch.cpp
@@ -1,124 +1,52 @@
-#include <cstdio>
-#include <cstdlib>
 #include <iostream>
-using std::string;
+#include <cstdio>
 using namespace std;
-static struct IO {
-	char tmp[1 << 10];
+#define gc getchar_unlocked
+void scanint(int &x);
 
-	// fast input routines
-	char cur;
+void scanint(int &x){
+	register int c = gc();
+	x = 0;
+	int neg = 0;
+	for(; ((c<48 || c>57) && c!='-'); c = gc());
+	if(c=='-'){ neg = 1; c = gc();}
+	for(; c>47 && c<58; c=gc()){ x = (x<<1)+(x<<3)+c-48; }
+	if(neg)	x=-x;
 
-//#define nextChar() (cur = getc_unlocked(stdin))
-//#define peekChar() (cur)
-	inline char nextChar() { return cur = getc_unlocked(stdin); }
-	inline char peekChar() { return cur; }
-
-	inline operator bool() { return peekChar(); }
-	inline static bool isBlank(char c) { return (c < '-' && c); }
-	inline bool skipBlanks() { while (isBlank(nextChar())); return peekChar() != 0; }
-
-	inline IO& operator >> (char & c) { c = nextChar(); return *this; }
-
-	inline IO& operator >> (char * buf) {
-		if (skipBlanks()) {
-			if (peekChar()) {
-				*(buf++) = peekChar();
-				while (!isBlank(nextChar())) *(buf++) = peekChar();
-			} *(buf++) = 0; } return *this; }
-
-	inline IO& operator >> (string & s) {
-		if (skipBlanks()) {	s.clear(); s += peekChar();
-			while (!isBlank(nextChar())) s += peekChar(); }
-		return *this; }
-
-	inline IO& operator >> (double & d) { if ((*this) >> tmp) sscanf(tmp, "%lf", &d); return *this;	}
-
-#define defineInFor(intType) \
-	inline IO& operator >>(intType & n) { \
-		if (skipBlanks()) { \
-			int sign = +1; \
-			if (peekChar() == '-') { \
-				sign = -1; \
-				n = nextChar() - '0'; \
-			} else \
-				n = peekChar() - '0'; \
-			while (!isBlank(nextChar())) { \
-				n += n + (n << 3) + peekChar() - 48; \
-			} \
-			n *= sign; \
-		} \
-		return *this; \
-	}
-
-defineInFor(int)
-defineInFor(unsigned int)
-defineInFor(long long)
-
-	// fast output routines
-
-//#define putChar(c) putc_unlocked((c), stdout)
-	inline void putChar(char c) { putc_unlocked(c, stdout); }
-	inline IO& operator << (char c) { putChar(c); return *this; }
-	inline IO& operator << (const char * s) { while (*s) putChar(*s++); return *this; }
-
-	inline IO& operator << (const string & s) { for (int i = 0; i < (int)s.size(); ++i) putChar(s[i]); return *this; }
-
-	char * toString(double d) { sprintf(tmp, "%lf%c", d, '\0'); return tmp; }
-	inline IO& operator << (double d) { return (*this) << toString(d); }
-
-
-#define defineOutFor(intType) \
-	inline char * toString(intType n) { \
-		char * p = (tmp + 30); \
-		if (n) { \
-			bool isNeg = 0; \
-			if (n < 0) isNeg = 1, n = -n; \
-			while (n) \
-				*--p = (n % 10) + '0', n /= 10; \
-			if (isNeg) *--p = '-'; \
-		} else *--p = '0'; \
-		return p; \
-	} \
-	inline IO& operator << (intType n) { return (*this) << toString(n); }
-
-defineOutFor(int)
-defineOutFor(long long)
-
-#define endl ('\n')
-#define cout __io__
-#define cin __io__
-} __io__;
-
-
+}
 int main(){
-	int t;
-	scanf("%d",&t);
-	int i;
-	char c1,c2;
-	c1=getchar_unlocked();
-	int count=0;
-	int cel=0;
+	int t,i,count=0,sum=0;
+	scanint(t);
+	char c;
+
 	for(i=0; i<t; i++){
-		c1= getchar_unlocked();
-		do{
-			c2 = getchar_unlocked();	
-			cel++;
-			if(c2 == c1){
+		c = gc();
+		while(c!='\n'){
+			if(c=='+'){
+				c = gc();
+				while(c!='\n' && c!='-'){
+					count++;
+					c = gc();
+				}
 				count++;
-				if(c2 == '+')
-					c2 = '-';
-				else
-					c2 = '+';
+				sum+=(count/2);
+				count=0;
 			}
-			c1 = c2;
-		} while(c2 != '\n' || c1 != '\n');
-		if(cel==3 && count==2)
-			count--;
-		printf("%d\n",count);
-		//printf("%d\n", count);
-		count =0;	cel=0;
+			else if(c=='-'){
+				c = gc();
+				while(c!='\n' && c!='+'){
+					count++;
+					c = gc();
+				}
+				count++;
+				sum+=(count/2);
+				count=0;
+			}
+		}
+		printf("%d\n",sum );
+		sum=0;
+	
 	}
 
-	return 1;
+	return 0;
 }
